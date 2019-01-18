808b792cb1840a4749611d442bbe855db8d61cb4
jj <xuexiaoqin@qq.com>
2019-01-18 10:59:16 +0800

    第三个功能

--- main.c
+++ main.c
@@ -73,6 +73,16 @@ int main()
 
         if (code == 3)
         {
+            printf("所有学生成绩如下：\n");
+
+            for (int i = 0; i < index; i++)
+            {
+                printf("第%d个学生%s的语文成绩为:%d数学成绩为:%d英语成绩为:%d\n", i + 1,arr[i].name,arr[i].yw, arr[i].sx, arr[i].yy);
+            }
+            printf("显示成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
 
         if (code == 4)
